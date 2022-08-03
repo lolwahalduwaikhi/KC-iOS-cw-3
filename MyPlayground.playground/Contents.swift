16bb2f23b6f22bf3cea3e8f030724a4ec4b02431
lolwahalduwaikhi <lolwah.alduwaikhi@gmail.com>
2022-08-03 19:37:45 +0300

    done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var favoriteNumber = 22
+var name:String = "Lulwah"
+var hobby = "playing the piano"
+let BirthofDate = 2004
+
+print("hello my name is \(name) and my favorite Number is \(favoriteNumber) and my hobby is \(hobby)\n and i was born at \(BirthofDate)")
---
+++ cw3/cw3/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  cw3
+//
+//  Created by Lulwah alduwaikhi on 03/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var username = ""
+    @State var fullname = ""
+    var body: some View {
+        ZStack{
+            VStack{
+                
+                Text("welcomeback,\t \(username)\(fullname)")
+                
+                Image("profile picture")
+                
+                TextField("enter your full name",text: $fullname)
+                
+                TextField("enter your username",text: $username)
+                
+        }
+           
+        }
+       
+             
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
